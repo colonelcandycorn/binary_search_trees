5ec905deb6332ba70f2fe1872ec4aa4f406a80ad
Sarah Dylan <sarahmdylan@gmail.com>
2022-07-26 14:09:24 -0700

    Add BFS methods
    Added an iterative and recursive method for BFS. The recursive one
    seems pretty inefficient or I'm missing the big picture somehow. I
    satisfied with the iterative one for now.

--- lib/tree.rb
+++ lib/tree.rb
@@ -51,12 +51,41 @@ class Tree
     node
   end
 
+  def find(data, node = @root)
+    return find(data, node.right) if data > node.data
+
+    return find(data, node.left) if data < node.data
+
+    return node if data == node.data
+  end
+
   def min_value(node)
     current_node = node
     while current_node.left
       current_node = current_node.left
     end
     current_node
+  end
+
+  def level_order(node = @root)
+    queue = []
+    array = []
+    queue.push(node)
+    until queue.empty?
+      current_node = queue[0]
+      queue.push(current_node.left) if current_node.left
+      queue.push(current_node.right) if current_node.right
+      block_given? ? (yield queue.shift) : array.push(queue.shift)
+    end
+    array
+  end
+
+  def level_order_recursion(node = @root, level = 0)
+    array = [[node, level]]
+    array += level_order_recursion(node.left, level + 1) if node.left
+    array += level_order_recursion(node.right, level + 1) if node.right
+    array.sort { |one, two| one[1] <=> two[1] } if level.zero?
+    block_given? ? array.each { |queue| yield queue[0] } : array
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
@@ -73,3 +102,10 @@ tree.insert(0)
 tree.pretty_print
 tree.delete(5)
 tree.pretty_print
+p tree.find(3)
+tree.level_order { |node| puts "#{node.data}\n" }
+array2 = tree.level_order_recursion
+array2.each { |array| puts "#{array[0].data}\n" }
+string = ''
+tree.level_order_recursion { |node| string += node.data.to_s }
+puts string
